c7934bd4a5772f669b3513168f59b870b7c53a39
sandeep-sandy-git <sandeep.x.sandy49@gmail.com>
2023-11-28 14:45:22 -0600

    added upload songs functionality and toast and more

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Sidebar from "@/components/Sidebar";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({ subsets: ["latin"] });
 
@@ -22,12 +23,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>
-              {children}
-            </Sidebar>
+            <Sidebar>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import AuthModal from "@/components/AuthModal";
+import UploadModal from "@/components/UploadModal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -15,7 +16,12 @@ const ModalProvider = () => {
     return null;
   }
 
-  return <><AuthModal /></>;
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+    </>
+  );
 };
 
 export default ModalProvider;
